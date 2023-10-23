cae085d722028dcef271ed628be804d349121db8
bnvillamor <benjosephvillamor@gmial.com>
2023-10-22 19:26:19 -0700

    filled out work experiences

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,10 +47,8 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
+        I am a software developer with experience in Python, React, JavaScript, HTML and CSS. 
+        I love serving my communbity and creating efficient solutions that solve
         real-world problems. Let's work together to bring your ideas to life!
       </motion.p>
 
